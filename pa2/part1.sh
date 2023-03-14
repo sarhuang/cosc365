f480e9cf79853dc64d4d1ed0ee8fe2ef8eb8defd
Sarah Huang <shuang24@hydra18.eecs.utk.edu>
2023-03-14 12:55:07 -0400

    accidentatlly deleted the file. this is why you use git

--- pa2/part1.sh
+++ pa2/part1.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
 # Your solution for part 1 goes here.
+
+for file in "part1_start"/*.txt; do
+
+	name=$(basename "$file" .txt_
+	mkdir "$name"
+
+	while read line || [ -n "$line"]; do
+		property=$(echo "$line" | awk '{print $1}')
+		value=$(echo "$line" | awk '{print $2}')
+
+		echo "$value" > "$name/$property.txt"
+	done < "$file"
+done
